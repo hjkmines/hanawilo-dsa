eaf919f7be45609761e0d02885b1ea399259a81b
e145278 <tony.kim@wnco.com>
2022-11-11 10:45:28 -0600

    test

--- Problem_Solving_Patterns/sliding_window.js
+++ Problem_Solving_Patterns/sliding_window.js
@@ -32,6 +32,7 @@ const maxSubarraySumLinear = (arr, num) => {
     let maxSum = 0;
     let tempSum = 0;
     if (arr.length < num) return null;
+    //calculate the initial window sum 
     for (let i = 0; i < num; i++) {
         maxSum += arr[i];
     }
@@ -39,27 +40,9 @@ const maxSubarraySumLinear = (arr, num) => {
     tempSum = maxSum;
 
     for (let i = num; i < arr.length; i++) {
+        // grabs the next sliding window [1, 2, 3] gets [2,3] instead of [1, 2]
         tempSum = tempSum - arr[i - num] + arr[i];
         maxSum = Math.max(maxSum, tempSum);
-    }
-    return maxSum;
-}
-
-
-// O(n)
-const minSubarraySumLinear = (arr, num) => {
-    let minSum = 0;
-    let tempSum = 0;
-    if (arr.length < num) return null;
-    for (let i = 0; i < num; i++) {
-        minSum += arr[i];
-    }
-
-    tempSum = maxSum;
-    
-    for (let i = num; i < arr.length; i++) {
-        tempSum = tempSum - arr[i - num] + arr[i];
-        minSum = Math.min(minSum, tempSum);
     }
     return maxSum;
 }
