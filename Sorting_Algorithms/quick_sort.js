43d72da142bc3ac31a1d782ecda5ef71e90e1b40
e145278 <tony.kim@wnco.com>
2022-11-18 22:54:41 -0600

    test

--- Sorting_Algorithms/quick_sort.js
+++ Sorting_Algorithms/quick_sort.js
@@ -1,6 +1,6 @@
 // Pivot Helper
 
-// In order to implement merge sort, it's useful to first implement a function 
+// In order to implement quick sort, it's useful to first implement a function 
 // responsible arranging elements in an array on either side of a pivot
 // Given an array, this helper function should designate an element as the pivot
 // It should then rearrange elements in the array so that all values less than 
